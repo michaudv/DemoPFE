9b3c15a9b107b6661337b610f20e0ede6cf5b1b1
bigotalex <alexandre.bigot9@gmail.com>
2017-01-26 17:33:16 +0100

    Demo screen Foyer

--- app/src/main/java/fr/lab/bbox/bboxapidemo/Demo/DemoFoyer.java
+++ app/src/main/java/fr/lab/bbox/bboxapidemo/Demo/DemoFoyer.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import fr.lab.bbox.bboxapirunner.R;
 
@@ -17,15 +18,36 @@ public class DemoFoyer extends Fragment {
 
     private final static String TAG = "DEMO_FOYER";
 
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.foyer_layout, container, false);
+        view = inflater.inflate(R.layout.foyer_layout, container, false);
+        return view;
     }
 
     @Override
     public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        ImageView foyer = (ImageView) view.findViewById(R.id.foyerView);
+        foyer.setImageResource(R.drawable.foyer);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
 
